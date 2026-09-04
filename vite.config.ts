1e4e5a7790b1d61f4e1c7b37ec4007a7a7e4f7be
JaniceOvando <janice@JaniceOvando.localdomain>
2026-09-03 20:11:39 -0600

    fix vite base path

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig({ base: '/ejercicios-react-janice/',
   plugins: [react()],
 })
